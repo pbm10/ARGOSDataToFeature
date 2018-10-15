f82dcd90e37774cefe23244c658baa1daa5209d5
pbm10 <42747774+pbm10@users.noreply.github.com>
2018-10-15 12:35:48 -0400

    Get tag ID

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -9,3 +9,29 @@
 ## Created: Fall 2018
 ## Author: John.Fay@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
+
+# Set input variables (Hard-wired)
+inputFile = 'V:/ARGOSTracking/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+outputFC = "V:/ARGOSTracking/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+#Open the ARGOS Data file for reading
+inputFileObj = open(inputFile,'r')
+
+#Get the first line so we can loop
+lineString = inputFileObj.readline()
+while lineString:
+    #Get code to run only if the line contains "Date :"
+    if "Date :" in lineString:
+
+        #Split the line string into a list
+        lineList = lineString.split()
+
+        #Get attributes from first line
+        tagID = lineList[0]
+        break
+    
+    #Get the next line
+    lineString = inputFileObj.readline()
